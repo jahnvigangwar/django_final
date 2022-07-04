162d91e1f92c1ef0082d186e9eafa0235537a4f3
Jahnvi Gangwar <59444913+jahnvigangwar@users.noreply.github.com>
2022-07-04 20:34:59 +0530

    Update admin.py

--- onlinecourse/admin.py
+++ onlinecourse/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-# <HINT> Import any new Models here
+# Importing any new Models here
 from .models import Course, Lesson, Instructor, Learner, Question, Choice, Submission
 
 # <HINT> Register QuestionInline and ChoiceInline classes here
